e73f7e5714989246511b710e609479c82429c73f
MauroBorzi <mauroborzi91@gmail.com>
2025-07-21 13:05:47 +0200

    minor fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,11 +4,14 @@ const imgList = document.getElementById(`img-list`)
 // dichiaro le variabili
 const linkPhoto = `https://lanciweb.github.io/demo/api/pictures/`
 
+// uso la chiamata axios per richiamare i valori presenti nell'api
 axios.get(linkPhoto).then((elem) => {
   const myPhoto = elem.data
 
+  // creo un ciclo for per generare una card per ogni elemento presente nell'api
   for (i = 0; i < myPhoto.length; i++) {
-    imgList.innerHTML += `<div class="col-12 col-md-6 col-lg-4">
+    imgList.innerHTML += `
+       <div class="col-12 col-md-6 col-lg-4">
           <div class="card rounded-0 position-relative">
             <div class="position-absolute top-0 start-50 translate-middle"><img src="./assets/img/pin.svg" alt=""></div>
             <img src="${myPhoto[i].url}" class="card-img-top rounded-0 myPadding img-fluid square-image" alt="...">
@@ -19,6 +22,4 @@ axios.get(linkPhoto).then((elem) => {
           </div>
         </div>`
   }
-
-
 })
